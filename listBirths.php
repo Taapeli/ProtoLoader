cb13a42c859a4a24cac2091c407299b89f83b3e2
jpek-m <juha.makelainen@iki.fi>
2014-11-04 22:01:21 +0200

    Yhteinen aloitussivu
    readBirth, readNames -sivut saa poistaa (jos tämä toimii)

--- listBirths.php
+++ listBirths.php
@@ -11,6 +11,10 @@ th,td { padding: 5px; }
 </head>
 
 <body>
+<div style="display: block; width: 100px; position: fixed;
+    top: 1em; right: 1em; color: #FFF;
+    background-color: #ddd;
+    text-align: center; padding: 4px; text-decoration: none;"><a href="index.php">Paluu</a></div>
 <h1>Haku syntymäajalla Taapeli-kannasta</h1>
 
 <?php
