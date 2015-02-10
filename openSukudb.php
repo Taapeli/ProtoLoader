7a3e0caf637be675727d19f2b42f7c342461437f
jpek-m <juha.makelainen@iki.fi>
2015-02-10 22:25:42 +0200

    Neo4j-palvelimen osoite ja portti

--- openSukudb.php
+++ openSukudb.php
@@ -1,4 +1,4 @@
 
-// Avataan sukudb
+// Avataan sukudb: ip-osoite ja portti
 
-      $sukudb = new Everyman\Neo4j\Client('localhost', 1337);
+      $sukudb = new Everyman\Neo4j\Client('10.50.8.204', 8080);
