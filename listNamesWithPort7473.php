8856c26ab6bbc07e05b6c15cda078f30c4a69978
Jorma Haapasalo <jorma.haapasalo@pp.inet.fi>
2015-02-10 19:08:28 +0200

    Fourteenth try

--- listNamesWithPort7473.php
+++ listNamesWithPort7473.php
@@ -20,7 +20,8 @@
     echo "<p>Poiminta nimi = '$input_name''$input_wildcard'</p>";
     $input_wildcard = $input_wildcard . ".*";
 
-    $sukudb = new Everyman\Neo4j\Client('taademo2.jelastic.elastx.net', 7473);
+//    $sukudb = new data\Everyman\Neo4j\Client('taademo2.jelastic.elastx.net', 7473);
+    $sukudb = new vendor\everyman\neo4jphp\lib\Everyman\Neo4j\Client('localhost', 7473);
 
     if ($input_name != '') {
       // Neo4j parameter {name} is used to avoid hacking injection
