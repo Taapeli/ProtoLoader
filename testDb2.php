b1fbf41e45d7b05e386960fa7a516ae387413ff2
jpek-m <juha.makelainen@iki.fi>
2015-02-12 10:18:29 +0200

    Tass 8080-portti

--- testDb2.php
+++ testDb2.php
@@ -14,7 +14,7 @@
 	  require('vendor/autoload.php');
 	  
 	  use Everyman\Neo4j\Client;
-	  $client = new Client('10.50.8.204', 7473);
+	  $client = new Client('10.50.8.204', 8080);
 
 	  print_r($client->getServerInfo()); 
 ?>
