6b604e5434d79186b13680c0b32e03a091f7d686
jpek-m <juha.makelainen@iki.fi>
2015-02-19 22:34:15 +0200

    Otsikko parannettu

--- alku.php
+++ alku.php
@@ -2,12 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fi" lang="fi">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>Taapelin prototyyppi</title>
+        <title>Taapelin prototyypin etusivu</title>
         <link rel="stylesheet" type="text/css" href="css/style.css" />
-        <style type="text/css">
-            #top { background-color: #82ac92; }
-        </style>
-    </head>
+     </head>
     <body>
         <?php include "inc/start.php"; ?>
         <div id="content">
