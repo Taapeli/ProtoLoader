b9c9e0042df3bc50a22cd046c58f998b17f50747
jpek-m <juha.makelainen@iki.fi>
2015-02-19 17:42:00 +0200

    Etusivun rakenne

---
+++ start.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "XHTML 1.0 Transitional" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fi" lang="fi">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Taapelin prototyyppi</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <style type="text/css">
+          div.top {
+              border-width: 0px;
+              padding: 25px;
+              background-color: lightblue;
+          }
+          div.bottom {
+              border-width: 0px;
+              padding: 5px;
+              background-color: slategray;
+              font-size: 70%;
+          }
+          div.menu {
+              border-width: 0px;
+              display: inline-block;
+              background-color: lightsteelblue;
+              padding: 10px;
+              vertical-align: top;
+              width: 15em;
+          }
+          div.content {
+              border-width: 0px;
+              display: inline-block;
+              padding: 10px;
+              vertical-align: top;
+          }
+          td { font-size: 9pt; }
+        </style>
+    </head>
+
+    <body>
+        <div class="top"><h1>Taapeli</h1></div>
+        <div class="menu"><?php include "menu.html"; ?></div>
+        <div class="content">TODO Sijoita tähän sisältö</div>
+        <div class="bottom">Suomen sukututkimusseura ry 2015</div>
+    </body>
+</html>
