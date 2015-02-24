8d9c43b133606a40c1e766374b2f902e76987805
jpek-m <juha.makelainen@iki.fi>
2015-02-24 22:43:33 +0200

    Yksi lisää

--- connectSameNames.php
+++ connectSameNames.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fi" lang="fi">
 <head>
+        <?php session_start(); ?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Taapeli haku</title>
+<title>Taapeli - samannimiset ehdokkaat</title>
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
 
 <body>
-<div  class="goback">
-  <a href="index.php">Paluu</a></div>
-<h1>Kytket&auml;&auml;n kaikki saman nimen omaavat</h1>
-<?php
 
-  include "inc/dbconnect.php";
+  <?php
+include 'checkUserid.php';
+include "inc/start.php";
+include 'classes/DateConv.php';
+include "inc/dbconnect.php";
 
-    
+        /*
+         * -- Content page starts here -->
+         */
 
-    $query_string = "MATCH (n:Person:user0498)-[:HAS_NAME]-(a), (m:Person:user6321)-[:HAS_NAME]-(b) WHERE ((a.first_name = b.first_name) AND (a.last_name = b.last_name)) RETURN n.id, m.id";
+    echo '<h1>Kytket&auml;&auml;n kaikki saman nimen omaavat</h1>';
+
+    $query_string = "MATCH (n:Person:user0498)-[:HAS_NAME]-(a), "
+            . "(m:Person:user6321)-[:HAS_NAME]-(b) "
+            . "WHERE ((a.first_name = b.first_name) AND (a.last_name = b.last_name)) "
+            . "RETURN n.id, m.id";
 
     $query = new Everyman\Neo4j\Cypher\Query($sukudb, $query_string);
     $result = $query->getResultSet();
+    $id = $id2 = [];
 
     foreach ($result as $rows)
     {
@@ -37,9 +46,9 @@
       $query = new Everyman\Neo4j\Cypher\Query($sukudb, $query_string);
       $result = $query->getResultSet();
     }
-    echo "May be the same connected: " . $may_be_same . "\n";
+    echo "<p>May be the same connected: " . $may_be_same . "</p>\n";
 
-?>
-
-</body>
-</html>
+  /*
+   * --- End of content page ---
+   */
+include "inc/stop.php";
