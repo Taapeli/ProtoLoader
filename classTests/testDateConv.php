31b73f399b9d87fa6cf0cfcd23213f2ab8b06292
jpek-m <juha.makelainen@iki.fi>
2015-03-12 16:34:05 +0200

    Nicer testDateConv output

--- classTests/testDateConv.php
+++ classTests/testDateConv.php
@@ -5,10 +5,22 @@
         <title>DateConv testi</title>
         <link rel="stylesheet" type="text/css" href="../style.css" />
         <style type="text/css">
+            body { background-color: aliceblue;
+                   background-image: url("../images/Vaakuna_65px.png");
+                   background-repeat: no-repeat;
+            }
+            h1,h2,h3 { margin-left: 65px; }
             .old { color: gray; }
             i { color: darkorange; font-style: normal; }
             b {color: saddlebrown; background-color: lightgray; }
             .error { color:red; }
+            div.inline {
+                display:inline; background-color: white;
+                width:48%;
+                border: 1px solid gray;	
+                float:left;
+                padding: 4px; margin: 2px;
+            }
         </style>
     </head>
 
@@ -25,10 +37,10 @@
             'FROM 1914 TO 1920', 'FROM 12 APR 1720 TO 15 JUN 1721', 
             'BEF SEP 1900', 'AFT 1820', 'AFT 5 DEC 1917',
             'BETW 24 JUN 1812 AND 31 AUG 1812',
-            ];
-        $dates = [ "1900-02-01", "1901-05-12", "1913-00-00", "1914-08-00",
+        ];
+        $dates = ["1900-02-01", "1913-00-00", "1914-08-00",
             "815-02-31", "1640-00-40", "1786.11.2", "1909.2.13", "1940.12.30"];
-        
+
 /* Tästä ei ole vielä tullut tolkkua ...
         function __autoload($class_name) {
           // Polku on ilman '../', jos ei olla testihakemistossa
@@ -39,28 +51,33 @@
         require_once(MAINDIR . 'libs/models/GedDateParser.php');
         require_once(MAINDIR . 'classes/DateConv.php');
 
-        echo "<h2>Päivämäärämuunnoksen testit</h2>\n";
-
-        echo "<h3>fromGed</h3>\n";
+        echo "<h2>Päivämäärämuunnoksen testit</h2>\n
+        <div>
+        <div class='inline'>
+        <h3>fromGed</h3>\n";
 
         $ged = new GedDateParser();
         foreach ($geddates as $s) {
           try {
-            echo "<p><b>" . $ged->fromGed($s) .
-            "</b> = GedDateParser->fromGed(<i>$s</i>)<br />\n";
+            $dgdate = $ged->fromGed($s);
+            echo "<p><b>$dgdate</b> = GedDateParser->fromGed(<i>$s</i>)<br />\n";
+            $dates[] = $dgdate; // Show also toDisplay
           } catch (Exception $e) {
             echo "<p><em class='error'>Error gedDateParser: " . $e->getMessage() .
-            "</em> = GedDateParser->fromGed(<i>$s</i>).<br />\n";
+                "</em> = GedDateParser->fromGed(<i>$s</i>).<br />\n";
           }
-          //echo "<span class='old'><b>" . DateConv::fromGed($s) .
-          //"</b> = DateConv::fromGed(<i>$s</i>)</span></p>\n";
+        //echo "<span class='old'><b>" . DateConv::fromGed($s) .
+        //"</b> = DateConv::fromGed(<i>$s</i>)</span></p>\n";
         }
-        
-        echo "<h3>toDisplay</h3>\n";
-        
+        echo "</div>
+        <div class='inline'>
+        <h3>toDisplay</h3>\n";
+
         foreach ($dates as $s) {
-          echo "<p><b>" . DateConv::toDisplay($s) .
-          "</b> = DateConv::toDisplay(<i>$s</i>)</p>\n";
+        echo "<p><b>" . DateConv::toDisplay($s) .
+        "</b> = DateConv::toDisplay(<i>$s</i>)</p>\n";
         }
         ?>
+        </div>
+      </div>
     </body></html>
